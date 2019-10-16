4a590b97bee829ee80f09c8d562438d396df61ba
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-16 15:13:58 -0400

    Updates

---
+++ precache-manifest.af309f6e1698513a56054649cac2fa09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bc28fb83dae36f8c2086cfd6cd3574",
+    "url": "/Deploy-React-App/index.html"
+  },
+  {
+    "revision": "2540a2cc1bd1eff1ad07",
+    "url": "/Deploy-React-App/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "16253faacb543d9c21cb",
+    "url": "/Deploy-React-App/static/js/2.c3b9ee50.chunk.js"
+  },
+  {
+    "revision": "2540a2cc1bd1eff1ad07",
+    "url": "/Deploy-React-App/static/js/main.6267291a.chunk.js"
+  },
+  {
+    "revision": "3bf4c50a75f594ef86ac",
+    "url": "/Deploy-React-App/static/js/runtime-main.ab62bb5b.js"
+  }
+]);
